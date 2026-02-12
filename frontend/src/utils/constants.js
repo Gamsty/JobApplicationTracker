e32a73bcc6e54676969d5d99524a79bc16d49f20
adriakg <adriakg@uio.no>
2026-02-12 08:45:02 +0100

    Create Constants and their respective colors.

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,17 @@
+export const APPLICATION_STATUS = {
+    APPLIED: 'Applied',
+    INTERVIEWING: 'Interviewing',
+    OFFER_RECEIVED: 'Offered',
+    REJECTED: 'Rejected',
+    WITHDRAWN: 'Withdrawn',
+    HIRED: 'Hired'
+};
+
+export const STATUS_COLORS = {
+    APPLIED: 'blue',
+    INTERVIEWING: 'orange',
+    OFFER_RECEIVED: 'green',
+    REJECTED: 'red',
+    WITHDRAWN: 'gray',
+    HIRED: 'purple'
+};
